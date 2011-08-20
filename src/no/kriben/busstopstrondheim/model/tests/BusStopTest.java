827d20416cc343076b75196bebe9799e72225410
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-08-20 08:18:47 +0200

    Initial commit.
    Added two domain model objects: BusStop and Position.

---
+++ src/no/kriben/busstopstrondheim/model/tests/BusStopTest.java
@@ -0,0 +1,31 @@
+package no.kriben.busstopstrondheim.model.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import no.kriben.busstopstrondheim.model.BusStop;
+import no.kriben.busstopstrondheim.model.Position;
+
+public class BusStopTest {
+	@Test
+    public void testBusStop() {
+		String name = "Spongdal";
+		String id = "123123123";
+		int code = 4444;
+        BusStop busStop = new BusStop(name, id, code);
+		assertEquals(busStop.getName(), name);
+		assertEquals(busStop.getId(), id);
+		assertEquals(busStop.getCode(), code);
+		assertNull(busStop.getPosition());
+    }
+	
+	@Test
+	public void testPosition() {
+		BusStop busStop = new BusStop("Some stop", "23233", 993);
+		Position position = new Position(63.4, 10.2);
+		busStop.setPosition(position);
+		assertEquals(position, busStop.getPosition());
+	}
+}
---
+++ src/no/kriben/busstopstrondheim/model/tests/PositionTest.java
@@ -0,0 +1,48 @@
+package no.kriben.busstopstrondheim.model.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import no.kriben.busstopstrondheim.model.Position;
+
+import org.junit.Test;
+
+
+public class PositionTest {
+	@Test
+	public void testConstruction()
+	{
+		double longitude = 10.0;
+        double latitude = 23.34;
+			
+		Position location = new Position(latitude, longitude);
+		assertEquals(longitude, location.getLongitude(), 0.000001);
+		assertEquals(latitude, location.getLatitude(), 0.000001);
+	}
+	
+	@Test
+	public void testPositionToSamePositionDistance()
+	{
+		ArrayList<Position> positions = new ArrayList<Position>();
+		positions.add(new Position(0.0, 0.0));
+		positions.add(new Position(100.0, 50.9));
+        positions.add(new Position(140.0, -134.0));
+		
+		for (Position p : positions) 
+			assertEquals(0.0f, p.distanceTo(p), 0.00001);
+	}
+	
+	@Test
+    public void testPositionExample() {
+        // Test data from http://williams.best.vwh.net/avform.htm
+        Position lax = new Position(33.942522, -118.407161);
+        Position jfk = new Position(40.639751, -73.778926);
+
+        //
+        float distanceInMeters = 2149.0f * 1.852f;
+        float delta = 10.0f;
+        assertEquals(distanceInMeters, lax.distanceTo(jfk), delta);
+        assertEquals(distanceInMeters, jfk.distanceTo(lax), delta);
+    }
+}
