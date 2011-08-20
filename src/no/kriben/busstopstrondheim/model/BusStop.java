46e16d4ac048a9c5ae86071d0a222b1277a261cf
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2011-08-20 08:30:48 +0200

    Clean up code (indentation).

--- src/no/kriben/busstopstrondheim/model/BusStop.java
+++ src/no/kriben/busstopstrondheim/model/BusStop.java
@@ -1,39 +1,39 @@
 package no.kriben.busstopstrondheim.model;
 
+public class BusStop {
+    private String name_;
+    private String id_;
+    private int code_;
+    private Position position_ = null;
 
-public class BusStop {	
-	private String name_;
-	private String id_;
-	private int code_;
-	private Position position_ = null;
-	
-	public BusStop(String name, String id, int code) {
-		name_ = name;
-		id_ = id;
-		code_ = code;
-	}
+    public BusStop(String name, String id, int code) {
+        name_ = name;
+        id_ = id;
+        code_ = code;
+    }
 
-	public String getName() {
-		return name_;
-	}
+    public String getName() {
+        return name_;
+    }
 
-	public String getId() {
-		return id_;
-	}
+    public String getId() {
+        return id_;
+    }
 
-	public int getCode() {
-		return code_;
-	}
-	
-	public String toString() {
-		return name_;
-	}
-	
-	public Position getPosition() {
-		return position_;
-	}
+    public int getCode() {
+        return code_;
+    }
 
-	public void setPosition(Position position) {
-		position_ = position;
-	}
+    @Override
+    public String toString() {
+        return name_;
+    }
+
+    public Position getPosition() {
+        return position_;
+    }
+
+    public void setPosition(Position position) {
+        position_ = position;
+    }
 }
--- src/no/kriben/busstopstrondheim/model/tests/PositionTest.java
+++ src/no/kriben/busstopstrondheim/model/tests/PositionTest.java
@@ -1,6 +1,6 @@
 package no.kriben.busstopstrondheim.model.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 
@@ -8,32 +8,29 @@ import no.kriben.busstopstrondheim.model.Position;
 
 import org.junit.Test;
 
-
 public class PositionTest {
-	@Test
-	public void testConstruction()
-	{
-		double longitude = 10.0;
+    @Test
+    public void testConstruction() {
+        double longitude = 10.0;
         double latitude = 23.34;
-			
-		Position location = new Position(latitude, longitude);
-		assertEquals(longitude, location.getLongitude(), 0.000001);
-		assertEquals(latitude, location.getLatitude(), 0.000001);
-	}
-	
-	@Test
-	public void testPositionToSamePositionDistance()
-	{
-		ArrayList<Position> positions = new ArrayList<Position>();
-		positions.add(new Position(0.0, 0.0));
-		positions.add(new Position(100.0, 50.9));
+
+        Position location = new Position(latitude, longitude);
+        assertEquals(longitude, location.getLongitude(), 0.000001);
+        assertEquals(latitude, location.getLatitude(), 0.000001);
+    }
+
+    @Test
+    public void testPositionToSamePositionDistance() {
+        ArrayList<Position> positions = new ArrayList<Position>();
+        positions.add(new Position(0.0, 0.0));
+        positions.add(new Position(100.0, 50.9));
         positions.add(new Position(140.0, -134.0));
-		
-		for (Position p : positions) 
-			assertEquals(0.0f, p.distanceTo(p), 0.00001);
-	}
-	
-	@Test
+
+        for (Position p : positions)
+            assertEquals(0.0f, p.distanceTo(p), 0.00001);
+    }
+
+    @Test
     public void testPositionExample() {
         // Test data from http://williams.best.vwh.net/avform.htm
         Position lax = new Position(33.942522, -118.407161);
