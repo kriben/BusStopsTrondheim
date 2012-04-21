e249f5a7efbe43cf293a2beb6eb1be7f886d4ff5
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2012-04-21 17:05:59 +0200

    Download more bus stops as default.

--- src/no/kriben/busstopstrondheim/io/ItalianSoapBusDepartureRepository.java
+++ src/no/kriben/busstopstrondheim/io/ItalianSoapBusDepartureRepository.java
@@ -17,7 +17,9 @@ public class ItalianSoapBusDepartureRepository implements
 
     private String username_;
     private String password_;
-
+    final private int NUM_HOURS = 6;
+    final private int MAX_DEPARTURES = 50;
+    
     public ItalianSoapBusDepartureRepository(String username, String password) {
         username_ = username;
         password_ = password;
@@ -37,8 +39,8 @@ public class ItalianSoapBusDepartureRepository implements
 
     public String getData(int busStopId) throws MalformedURLException {
 
-        String SOAP_ACTION = "http://miz.it/infotransit/getUserRealTimeForecastByStop";
-        String METHOD_NAME = "getUserRealTimeForecastByStop";
+        String SOAP_ACTION = "http://miz.it/infotransit/getUserRealTimeForecastExByStop";
+        String METHOD_NAME = "getUserRealTimeForecastExByStop";
         String NAMESPACE = "http://miz.it/infotransit";
         String URL = "http://st.atb.no/InfoTransit/userservices.asmx";
         try {
@@ -53,7 +55,9 @@ public class ItalianSoapBusDepartureRepository implements
             // Set the bus stop id property
             request.addProperty(pi);
             request.addProperty("busStopId", new Integer(busStopId).toString());
-
+            request.addProperty("nForecast", MAX_DEPARTURES);
+            request.addProperty("nHours", NUM_HOURS);
+            
             SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
                     SoapEnvelope.VER11);
             envelope.dotNet = true;
@@ -61,10 +65,10 @@ public class ItalianSoapBusDepartureRepository implements
             envelope.addMapping(NAMESPACE, "WsAuthentication",
                     new Auth().getClass());
             HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-            androidHttpTransport.debug = true;
+            //androidHttpTransport.debug = true;
             androidHttpTransport.call(SOAP_ACTION, envelope);
-            // System.out.println(androidHttpTransport.requestDump);
-            // System.out.println(androidHttpTransport.responseDump);
+            //System.out.println(androidHttpTransport.requestDump);
+            //System.out.println(androidHttpTransport.responseDump);
 
             SoapPrimitive result = (SoapPrimitive) envelope.getResponse();
             return result.toString();
